99c03dd80603c5ff458714d77d902f7e2970817c
Ryan Alqasemi <alqasemir@usf.edu>
2025-03-27 19:22:32 -0400

    Add fetchProductsAsync using async/await and try/catch

--- cc_16.js
+++ cc_16.js
@@ -1,6 +1,6 @@
 //Coding Challenge #16
 
-//Task 2
+//Task 2 Fetch products with .then()
 function fetchProductsThen() { //creates fetch function
     fetch('https://www.course-api.com/javascript-store-products') //retrieved api data
       .then(response => { //checks for errors
@@ -19,3 +19,20 @@ function fetchProductsThen() { //creates fetch function
         console.error('Error fetching products with .then():', error);
       });
   } 
+
+  //Task 3 Fetching products with async/await
+
+  async function fetchProductsAsync() { //Creates async function
+    try {
+      const response = await fetch('https://www.course-api.com/javascript-store-products');
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      const products = await response.json();
+      // Call displayProducts to render products
+      displayProducts(products);
+    } catch (error) {
+      // Pass any errors to the error handler
+      handleError(error);
+    }
+  }
